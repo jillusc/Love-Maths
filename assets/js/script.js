844aa25cce4fc5c8929d89d46dd09e69b2f241db
jillusc <>
2023-09-13 21:32:41 +0000

    Added division game code as challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,6 +99,10 @@ function runGame(gameType) {
 
         displaySubtractQuestion(num1, num2);
 
+    } else if (gameType === "division") {
+
+        displayDivisionQuestion(num1, num2);
+
     } else {
 
         alert(`Unknown game type: ${gameType}`);
@@ -197,6 +201,10 @@ function calculateCorrectAnswer() {
     } else if (operator === "-") {
 
         return [operand1 - operand2, "subtract"];
+
+    } else if (operator === "/") {
+
+        return [operand1 / operand2, "division"];
 
     } else {
 
@@ -310,6 +318,20 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 
 
+
+
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+
+    operand1 = operand1 * operand2;
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+
+    document.getElementById('operator').textContent = "/";
 
 
 }
